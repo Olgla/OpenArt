0abd0f94e9836608b84e28294e3dc5de29c8d26c
Olgla <elenagladenko@gmail.com>
2025-02-05 10:25:43 -0500

    total pages pagination

--- script.js
+++ script.js
@@ -6,55 +6,41 @@ const baseUrlInfo = "https://api.artic.edu/api/v1";
 const baseImageUrl = "https://www.artic.edu/iiif/2";
 let parameters = "fields=id,title,image_id,gallery_title,artwork_type,department_title,artist_title,category_title,technique_titles";
 
-fetch(baseUrlInfo + "/artworks?" + parameters + "&limit=100")
+let totalPages = 1;
+
+fetch(baseUrlInfo + "/artworks")
   .then(response => response.json())
   .then(data => {
-    console.log(data.data);
+    totalPages = data.pagination.total_pages;
+    getArtworks()
+  })
+  .catch (error => console.error(error));
+
+
+async function getArtworks() {
+  let randomPage = Math.floor(Math.random() * totalPages) + 1;
+  try {
+    const response = await fetch(`${baseUrlInfo}/artworks?page=${randomPage}&${parameters}&limit=20`);
+    const data = await response.json();
+    console.log(data.data); 
+
     data.data.forEach(artwork => {
-      if (artwork.image_id) {  
+      if (artwork.image_id) {
         const item = document.createElement('img');
         item.classList.add('artwork-item');
-        
         item.src = `${baseImageUrl}/${artwork.image_id}/full/full/0/default.jpg`;
         item.alt = artwork.title;
+
+        item.addEventListener('click', () => {
+          const imageUrl = `${baseImageUrl}/${artwork.image_id}/full/full/0/default.jpg`;
+          window.open(imageUrl, '_blank'); 
+        });
 
         list.appendChild(item);
       }
     });
     body.appendChild(list);
-  })
-  .catch(error => console.log(error));
-           
- 
-
-
-
-
-
-// artworkDetails = {
-//     "Title": artwork.title,
-//     "Artist": artwork.artist_title,
-//     "Type": artwork.artwork_type,
-//     "Technique": artwork.technique_titles.join(", "),
-//     "Gallery": artwork.gallery_title,
-//     "Department": artwork.department_title,
-//     "Category": artwork.category_title,
-//     "imageId": artwork.image_id,
-//     // "AltText": artwork.alt_text
-// };   
-// images.push(artworkDetails);
-// console.log(images);                              
-// });            
-// })
-// .catch(error => console.log(error));
-
-  
-
-// 
-
-
-// fetch(`${baseImageUrl}/${artworkDetails.imageId}/full/full/0/default.jpg`)
-// .then(response => {
-// JSON.parse(response);
-// console.log(response)
-// })
+  } catch (error) {
+    console.log('Error fetching artworks:', error);
+  }
+}
